d31309b25051acf6f9009284851395bb67f1b8f1
simosco <101003922+si-mosco@users.noreply.github.com>
2022-03-16 18:16:49 +0100

    Update Program.cs

--- funcion1Console/Program.cs
+++ funcion1Console/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +13,10 @@ namespace funcion1Console
             string funzione;
             int[] PezziFunzione = new int[100];
             string[] SingoliPezziFunzione = new string[100];
-            int indice = 1, contatore1=0;
+            int indice = 1, contatore1=0,aumento=0;
+            bool[] x = new bool[100];
+            bool[] x2 = new bool[100];
+            string[] elevazioni= new string[100];
 
             funzione = Console.ReadLine();
             PezziFunzione[0] = 0;
@@ -21,17 +24,46 @@ namespace funcion1Console
             {
                 if (funzione.Substring(i, 1) == "+" || funzione.Substring(i, 1) == "-" || funzione.Substring(i, 1) == "*" || funzione.Substring(i, 1) == "/")
                 {
-                    PezziFunzione[indice] = i;
+                    PezziFunzione[indice] = i; //qui ci sono operatori
                     indice++; //quantità di operatori nella funzione
                 }
             }
-            while (contatore1 <= indice) //mi salvo in un array di stringe ogni sottostringa in cui ho suddiviso la mia funzione
+            PezziFunzione[indice] = funzione.Length;
+            /*for (int i = 0; i < indice; i++)
             {
-                SingoliPezziFunzione[contatore1] = funzione.Substring(PezziFunzione[contatore1], (PezziFunzione[contatore1 + 1] - PezziFunzione[contatore1]));
+                Console.WriteLine(PezziFunzione[i]);
+            }*/
+            Console.ReadKey();
+            while (contatore1 < indice) //mi salvo in un array di stringe ogni sottostringa in cui ho suddiviso la mia funzione
+            {
+                if (contatore1 > 0)
+                    aumento = 1;
+                SingoliPezziFunzione[contatore1] = funzione.Substring(PezziFunzione[contatore1]+aumento, (PezziFunzione[contatore1 + 1] - PezziFunzione[contatore1]-aumento));
                 contatore1++;
             }
+            
+            for (int i=0; i<SingoliPezziFunzione.Length; i++)//per ogni parola, confronto ogni lettera se uguale a x (presente=vero| non presente=falso)
+            {
+                for (int j=0; j<SingoliPezziFunzione[i].Length; j++)
+                {
+                    if (SingoliPezziFunzione[i].Substring(j,1).ToUpper()=="X")
+                    {
+                        x[i]=true;
+                    }
+                }
+                for (int j=0; j<SingoliPezziFunzione[i].Length-1; j++)
+                {
+                    if (SingoliPezziFunzione[i].Substring(j, 2).ToUpper() == "X^")
+                    {
+                        x2[i] = true;
+                        elevazioni[i]=SingoliPezziFunzione[i].Substring(j, (PezziFunzione[i + 1] - PezziFunzione[i] - 1));
+                        Console.WriteLine("elevazione"+ i+1);
+                        Console.WriteLine(elevazioni[i]);
+                    }
+                }
+            }
 
-            for (int i=0; i<100; i++)
+            for (int i=0; i<10; i++)
             {
                 Console.WriteLine(SingoliPezziFunzione[i]);
             }
